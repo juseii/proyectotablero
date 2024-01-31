a36248ee98c72bde3b4e4d25b2c95b1b4d3cb71d
juseii <137000379+juseii@users.noreply.github.com>
2024-01-30 22:36:08 -0600

    Add files via upload

---
+++ Hilos/src/clases/Proceso1.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Proceso1 extends Thread{
+     public void run(){
+    for(int i=0;i<=5;i++){
+
+    System.out.println("proceso 1");
+  }
+ }
+}
---
+++ Hilos/src/clases/Proceso2.java
@@ -0,0 +1,10 @@
+package clases;
+//Juan pablo almeida silvan
+public class Proceso2 extends Thread{    //metodo\
+     public void run(){
+for(int i=0;i<=5;i++){
+
+    System.out.println("proceso 2");
+  }
+ }
+}
